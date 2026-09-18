64079365b516c696cfc74e16c014f23fe73c0c6a
antonioallomedia-blip <antonio.allomedia@gmail.com>
2026-09-18 10:25:23 +0300

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,33 +1,14 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-}
-
 // Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
+    description = "Hanime.tv provider for CloudStream."
+    authors = listOf("antonioallomedia-blip")
+    status = 1
     tvTypes = listOf("Movie")
-
     requiresResources = true
     language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    iconUrl = "https://hanime.tv/favicon.ico"
 }
 
 android {
